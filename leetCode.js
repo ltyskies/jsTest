c6e2a152e9a668050db25c768cb70edd4b2620dc
lty <3305651861@qq.com>
2025-12-20 19:46:39 +0800

    前端手撕题

--- leetCode.js
+++ leetCode.js
@@ -1,3 +1,12 @@
 var minDeletionSize = function(strs) {
-    
+    let ans = 0
+    for(let i = 0;i<strs[0].length;i++){
+        for(let j = 1;j<strs.length;j++){
+          if(strs[j][i]<strs[j-1][i]){
+              ans++
+              break
+          }
+        }
+    }
+    return ans
 };
