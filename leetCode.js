5803dd0a59718d529266ee33eec289d5cba4ce9c
lty <3305651861@qq.com>
2025-12-20 20:04:53 +0800

    前端手撕题

--- leetCode.js
+++ leetCode.js
@@ -1,11 +1,44 @@
-var copyRandomList = function(head, cachedNode = new Map()) {
-    if (head === null) {
-        return null;
+const merge = (head1, head2) => {
+    const dummyHead = new ListNode(0);
+    let temp = dummyHead, temp1 = head1, temp2 = head2;
+    while (temp1 !== null && temp2 !== null) {
+        if (temp1.val <= temp2.val) {
+            temp.next = temp1;
+            temp1 = temp1.next;
+        } else {
+            temp.next = temp2;
+            temp2 = temp2.next;
+        }
+        temp = temp.next;
     }
-    if (!cachedNode.has(head)) {
-        cachedNode.set(head, {val: head.val}), Object.assign(cachedNode.get(head), 
-        {next: copyRandomList(head.next, cachedNode), 
-          random: copyRandomList(head.random, cachedNode)})
+    if (temp1 !== null) {
+        temp.next = temp1;
+    } else if (temp2 !== null) {
+        temp.next = temp2;
     }
-    return cachedNode.get(head);
+    return dummyHead.next;
 }
+
+const toSortList = (head, tail) => {
+    if (head === null) {
+        return head;
+    }
+    if (head.next === tail) {
+        head.next = null;
+        return head;
+    }
+    let slow = head, fast = head;
+    while (fast !== tail) {
+        slow = slow.next;
+        fast = fast.next;
+        if (fast !== tail) {
+            fast = fast.next;
+        }
+    }
+    const mid = slow;
+    return merge(toSortList(head, mid), toSortList(mid, tail));
+}
+
+var sortList = function(head) {
+    return toSortList(head, null);
+};
