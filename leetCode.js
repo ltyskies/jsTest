257769f12386dd17f13a942e0eb83dd435cad0e4
lty <3305651861@qq.com>
2025-12-20 20:01:14 +0800

    前端手撕题

--- leetCode.js
+++ leetCode.js
@@ -1,12 +1,11 @@
-var minDeletionSize = function(strs) {
-    let ans = 0
-    for(let i = 0;i<strs[0].length;i++){
-        for(let j = 1;j<strs.length;j++){
-          if(strs[j][i]<strs[j-1][i]){
-              ans++
-              break
-          }
-        }
+var copyRandomList = function(head, cachedNode = new Map()) {
+    if (head === null) {
+        return null;
     }
-    return ans
-};
+    if (!cachedNode.has(head)) {
+        cachedNode.set(head, {val: head.val}), Object.assign(cachedNode.get(head), 
+        {next: copyRandomList(head.next, cachedNode), 
+          random: copyRandomList(head.random, cachedNode)})
+    }
+    return cachedNode.get(head);
+}
