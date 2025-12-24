f237bf7c89c691f4897db6f926288aaaeb520748
lty <3305651861@qq.com>
2025-12-24 15:30:36 +0800

    前端手撕题

--- myAjax.js
+++ myAjax.js
@@ -1,16 +1,16 @@
 function myAjax(url){
   return new Promise((resolve,reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET',url,true)
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET",url,true);
     xhr.onreadystatechange = function(){
       if(xhr.readyState === 4){
-        if(xhr.status === 200){
-          resolve(JSON.stringify(xhr.responseText))
+        if(xhr.status >=200 && xhr.status <300){
+          resolve(xhr.responseText);
         }else{
-          reject('error')
+          reject(new Error(xhr.statusText));
         }
       }
     }
-    xhr.send()
+    xhr.send();
   })
 }
