3d889dbbe5a549fa7be33cf226182a6268b22df8
lty <3305651861@qq.com>
2026-02-24 10:26:56 +0800

    前端手撕题

--- leetCode.js
+++ leetCode.js
@@ -1,5 +1,16 @@
-for(var i = 0;i<3;i++){
-  setTimeout(function () {
-    console.log(i)
-  },1000)
-}
+var sumRootToLeaf = function(root) {
+    let sum = 0
+    function dfs(node,str){
+       if(node === null){
+           return
+       }
+       if(node.left === null && node.right === null){
+          sum += parseInt(str + node.val,2)
+          return
+       }
+        dfs(node.left,str + node.val)
+        dfs(node.right,str + node.val)
+    }
+    dfs(root,'')
+    return sum
+};
